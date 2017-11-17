9e106cb7800365fac516fc45e62d5cfe4a72e532
xandelly <asa-008@hotmail.com>
2017-11-17 20:26:01 -0200

    Automovel.java

--- Winder Rezende/Automovel.java
+++ Winder Rezende/Automovel.java
@@ -10,6 +10,7 @@ public class Automovel {
     private String placa;
     private String chassi;
     private int ano;
+
     //Contrutor com agumentos
     public Automovel(int ID, String fabricante, String modelo, String cor, String placa, String chassi, int ano){
         this.ID = ID;
@@ -20,67 +21,11 @@ public class Automovel {
         this.chassi = chassi;
         this.ano = ano;
     }
+
    //Contrutor Padrão
    public Automovel(){
        
    }
-
-//    public int getID() {
-//        return ID;
-//    }
-//
-//    public void setID(int ID) {
-//        this.ID = ID;
-//    }
-//
-//    public String getFabricante() {
-//        return fabricante;
-//    }
-//
-//    public void setFabricante(String fabricante) {
-//        this.fabricante = fabricante;
-//    }
-//
-//    public String getModelo() {
-//        return modelo;
-//    }
-//
-//    public void setModelo(String modelo) {
-//        this.modelo = modelo;
-//    }
-//
-//    public String getCor() {
-//        return cor;
-//    }
-//
-//    public void setCor(String cor) {
-//        this.cor = cor;
-//    }
-//
-//    public String getPlaca() {
-//        return placa;
-//    }
-//
-//    public void setPlaca(String placa) {
-//        this.placa = placa;
-//    }
-//
-//    public String getChassi() {
-//        return chassi;
-//    }
-//
-//    public void setChassi(String chassi) {
-//        this.chassi = chassi;
-//    }
-//
-//    public int getAno() {
-//        return ano;
-//    }
-//
-//    public void setAno(int ano) {
-//        this.ano = ano;
-//    }
-
 
     public String ImpAutomovel() {
         return "Identificação: " + ID + "\nFabricante: " + fabricante + "\nModelo: " + modelo + "\nCor: " + cor + "\nPlaca: " + placa + "\nChassi: " + chassi + "\nAno: " + ano+"\n";
