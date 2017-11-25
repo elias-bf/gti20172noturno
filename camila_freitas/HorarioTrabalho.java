c30456f45405b0662877b5c853522c8e7fe92ec9
Camila Freitas <camiladonascimentofreitas@gmail.com>
2017-11-25 02:41:43 -0200

    Atividade 01 sobre git e github

---
+++ camila_freitas/HorarioTrabalho.java
@@ -0,0 +1,12 @@
+package exercicio0811.classes;
+
+public class HorarioTrabalho{
+
+	public String horaEntrada;
+	public String horaSaida;
+
+	public HorarioTrabalho (String horaEntrada, String horaSaida){
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+	}
+}
